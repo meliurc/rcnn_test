ee62400ccd8803ce36029509d0b8a88f6c48143b
liurc <liurc@jpush.cn>
2018-07-28 17:57:51 +0800

    add make_dataset.py

--- make_dataset.py
+++ make_dataset.py
@@ -18,9 +18,9 @@ def gen_dataset_file(image_class_info_file_path, image_file_path, origin_file, d
 if __name__ == '__main__':
     object_class = "dog"
     train_file = object_class + "_train.txt"
-    test_file = object_class + "_val.txt"
     trainval_file = object_class + "_trainval.txt"
+    test_file = object_class + "_val.txt"
 
     gen_dataset_file(image_class_info_file_path, image_file_path, train_file, train_file)
-    gen_dataset_file(image_class_info_file_path, image_file_path, test_file, train_file)
-    gen_dataset_file(image_class_info_file_path, image_file_path, trainval_file, train_file)
+    gen_dataset_file(image_class_info_file_path, image_file_path, trainval_file, trainval_file)
+    gen_dataset_file(image_class_info_file_path, image_file_path, test_file, test_file)
